31e1d893ba83847e5eb59e7d4478b296e295f41c
discordboyy <mertygrul.28@gmail.com>
2026-04-15 13:30:22 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,15 @@ import logo from "../assets/header/UKF_logo_red.svg";
 import "../style/header.css";
 
 export default function Header() {
+
+  const scrollToContact = (e) => {
+    e.preventDefault(); // Forhindre standard oppførsel for lenken
+    const contactSection = document.getElementById("contact-section-id");
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <header>
       <Link to="/">
@@ -16,7 +25,9 @@ export default function Header() {
           <Link to="/hva-skjer" className="nav-item">hva skjer</Link>
           <Link to="/bestill-kurs" className="nav-item">bestill kurs</Link>
           <Link to="/prosjekter" className="nav-item">prosjekter CV</Link>
-          <a href="#contact-section-id" className="nav-item">kontakter</a>
+          <a href="#contact-section-id" className="nav-item" onClick={scrollToContact}>
+            kontakter
+          </a>
         </div>
       </div>
     </header>
