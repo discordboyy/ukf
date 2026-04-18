5428789afd8819614dbed7d413b815fa4cf0a45b
discordboyy <mertygrul.28@gmail.com>
2026-04-18 11:43:28 +0200

    upload

--- src/pages/Prosjekter.jsx
+++ src/pages/Prosjekter.jsx
@@ -18,7 +18,6 @@ import { Pagination, Navigation } from "swiper/modules";
 // Sentralen 2026 images
 import sentralen2026_img1 from "../assets/prosjekter/Sentralen-2026/img-1.jpg";
 import sentralen2026_img2 from "../assets/prosjekter/Sentralen-2026/img-2.jpg";
-import sentralen2026_img3 from "../assets/prosjekter/Sentralen-2026/img-3.jpg";
 import sentralen2026_img4 from "../assets/prosjekter/Sentralen-2026/img-4.jpg";
 import sentralen2026_img5 from "../assets/prosjekter/Sentralen-2026/img-5.jpg";
 import sentralen2026_img6 from "../assets/prosjekter/Sentralen-2026/img-6.jpg";
@@ -35,7 +34,6 @@ import sentralen2026_img14 from "../assets/prosjekter/Sentralen-2026/img-14.jpg"
 const sentralen2026Images = [
   sentralen2026_img1,
   sentralen2026_img2,
-  sentralen2026_img3,
   sentralen2026_img4,
   sentralen2026_img5,
   sentralen2026_img6,
@@ -82,6 +80,84 @@ export default function Prosjekter() {
   return (
     <div className="global-holder-container">
 
+      <div className="project-section last">
+        <div className="project-container">
+          <img className="project-icon blue" src={rombBlue} alt="Project icon" />
+
+          <div className="project-content">
+            <div className="project-header">
+
+              <div className="project-meta blue">
+                Desember 2025 – mars 2026
+              </div>
+
+              <div className="project-meta blue">
+                Barnas Kunstklubb i Bærum og Lørenskog
+              </div>
+
+              <h2 className="project-title blue" style={{ textTransform: "uppercase" }}>
+                Barnas Kunstklubb: kreative verksteder for barn og familier
+              </h2>
+
+            </div>
+
+            <div className="project-description-block">
+              <img className="project-image-placeholder" src={frogner1} alt="" />
+
+              <p className="project-descriptio">
+                <span>
+                  <span className="text-highlight blue">
+                    I løpet av 4 måneder har vi arrangert samlinger for barn og deres familier på Lørenskog Bibliotek og Bærum Frivilligsentral. 
+                    Samlingene var organisert som workshops i ulike teknikker innen kunst og håndverk. 
+                    Vi har hatt 13 samlinger hvert sted, og har dokumentert{" "}
+                  </span>
+
+                  <span className="text-accent red">
+                    144 besøk
+                  </span>
+
+                  <span className="text-highlight blue">
+                    av en mangfoldig gruppe deltakere. <br /><br />
+                    Prosjektet var støttet av Akershus fylkeskommune.
+                  </span>
+                </span>
+              </p>
+            </div>
+
+          </div>
+        </div>
+
+        <div className="project-slider">
+          <Swiper
+            slidesPerView="auto"
+            spaceBetween={48}
+            centeredSlides={true}
+            loop={true}
+            pagination={{ clickable: true, dynamicBullets: true }}
+            navigation={true}
+            modules={[Pagination]}
+          >
+
+            <SwiperSlide>
+              <img src={frogner6} alt="" />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img src={frogner3} alt="" />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img src={frogner4} alt="" />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img src={frogner5} alt="" />
+            </SwiperSlide>
+
+          </Swiper>
+        </div>
+      </div>
+
       <div className="project-section">
         <div className="project-container">
           <img className="project-icon red" src={rombRed} alt="Project icon" />
@@ -102,9 +178,9 @@ export default function Prosjekter() {
                   <span className="text-highlight red">
                     Under Håndverksdagen 2026 holdt vi et kreativt toveverksted med kardet ull for barn og voksne. 
                     Av tovede fargerike ullkuler kunne deltakerne lage sin egen unike nøkkelring eller pynt til veske og sekk. 
-                    Verkstedet samlet mange familier med barn, vi har telt  
+                    Verkstedet samlet mange familier med barn, vi har telt{" "}
                   </span>
-                  <span className="text-accent blue"> over 50 deltakere </span>
+                  <span className="text-accent blue">over 50 deltakere </span>
                   <span className="text-highlight red">i ulike aldre.</span>
                 </span>
               </p>
@@ -215,7 +291,6 @@ export default function Prosjekter() {
       </div>
 
       {/* SECOND PROJECT */}
-
       <div className="project-section last">
         <div className="project-container">
           <img className="project-icon blue" src={rombBlue} alt="Project icon" />
