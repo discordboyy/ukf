701dadc8d75fa5a1e37b83f8b326625a63466d72
discordboyy <mertygrul.28@gmail.com>
2026-08-29 08:55:39 +0200

    css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -68,7 +68,7 @@ export default function Footer() {
               <div className="about-details-content">DEN UKRAINSKE KUNSTFORENING</div>
               <div className="kontonummer">Org. n. 935 633 001</div>
 
-              <a href="mailto:ua.kunstforening@gmail.com" className="contact-email no-select">
+              <a href="mailto:ua.kunstforening@gmail.com" className="contact-email no-select" id="contact-email-id-special">
                 <span className="line">ua.kunstforening</span>
                 <span className="dog">@</span>
                 <span className="line">gmail.com</span>
